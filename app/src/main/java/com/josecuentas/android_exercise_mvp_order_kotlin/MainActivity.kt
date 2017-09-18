3dbec2681e338ff61f75c8b598df477dabbf2bc0
José Cuentas Turpo <jcuentast@gmail.com>
2017-09-18 10:50:29 -0500

    feat(setup): create project

---
+++ app/src/main/java/com/josecuentas/android_exercise_mvp_order_kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.josecuentas.android_exercise_mvp_order_kotlin
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
